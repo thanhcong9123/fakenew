1bbacaf4b5efcf6517a09b1b5f7bece1da3b3fab
thanhcong9123 <128565140+thanhcong9123@users.noreply.github.com>
2025-04-20 20:56:31 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+declare var bootstrap: any; // 👈 thêm dòng này!
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fakenews';
+  models = ['Logistic Regression', 'Random Forest', 'LSTM', 'BiLSTM'];
+  selectedModel = this.models[0];
+  title = '';
+  content = '';
+  result: string | null = null;
+  accuracy: number = 0;
+
+  selectModel(model: string) {
+    this.selectedModel = model;
+  }
+
+  kiemTraTin() {
+    // Giả lập kết quả
+    const isReal = Math.random() > 0.5;
+    this.result = isReal ? 'Tin thật' : 'Tin giả';
+
+    // Giả lập độ chính xác nếu là LSTM hoặc BiLSTM
+    if (this.selectedModel === 'LSTM' || this.selectedModel === 'BiLSTM') {
+      this.accuracy = Math.floor(80 + Math.random() * 20);
+    } else {
+      this.accuracy = 0;
+    }
+
+    const modalEl = document.getElementById('resultModal');
+    if (modalEl) {
+      const modal = new bootstrap.Modal(modalEl);
+      modal.show();
+    }
+  }
+
+  hasAccuracy(): boolean {
+    return this.selectedModel === 'LSTM' || this.selectedModel === 'BiLSTM';
+  }
 }
